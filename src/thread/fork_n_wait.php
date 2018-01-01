933ccf81659fde08a7da28bcc88bce1ab3e97106
fangyi-chuangcache <fangyi@aocyun.com>
2018-01-01 20:25:47 +0800

    UPDATE THREAD

--- src/thread/fork_n_wait.php
+++ src/thread/fork_n_wait.php
@@ -1,23 +1,20 @@
 <?php
-while (1)//循环采用3个进程
-{
-    //declare(ticks=1);
-    $bWaitFlag = false; // 是否等待进程结束
-    //$bWaitFlag = TRUE; // 是否等待进程结束
-    $intNum = 3; // 进程总数
-    $pids = []; // 进程PID数组
+$bWaitFlag = false;
+$intNum = 3;
+while (1) {
+    $pids = [];
     for ($i = 0; $i < $intNum; $i++) {
-        $pids[$i] = pcntl_fork();// 产生子进程，而且从当前行之下开试运行代码，而且不继承父进程的数据信息
-        /*if($pids[$i])//父进程
-        {
-        //echo $pids[$i]."parent"."$i -> " . time(). "\n";
+        $pids[$i] = pcntl_fork();
+        //父进程
+        if ($pids[$i]) {
+            echo $pids[$i] . " - parent" . "$i -> " . time() . PHP_EOL;
         }
-        */
+
         if ($pids[$i] == -1) {
-            echo "couldn't fork" . "\n";
+            echo "couldn't fork" . PHP_EOL;
         } else if (!$pids[$i]) {
             sleep(1);
-            echo "\n" . "第" . $i . "个进程 -> " . time() . "\n";
+            echo "第" . $i . "个进程 -> " . time() . PHP_EOL;
             //$url=" 抓取页面的例子
             //$content = file_get_contents($url);
             //file_put_contents('message.txt',$content);
@@ -26,7 +23,7 @@ while (1)//循环采用3个进程
         }
         if ($bWaitFlag) {
             pcntl_waitpid($pids[$i], $status, WUNTRACED);
-            echo "wait $i -> " . time() . "\n";
+            echo "wait $i -> " . time() . PHP_EOL;
         }
     }
     sleep(1);
